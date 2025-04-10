c78f281e4857311e5f29b682c6219539f6d2a28a
mweisel74 <michael@demymedia.com>
2025-04-09 21:51:27 -0400

    first commit

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,11 @@
+import { defineAuth } from '@aws-amplify/backend';
+
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome To Michael's Very First Amplify Application",
+      verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`
+    }
+  }
+});
